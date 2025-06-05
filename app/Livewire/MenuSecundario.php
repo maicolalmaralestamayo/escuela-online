f702368bba067459d74bae7b97890934a8da616e
Maicol Almarales Tamayo <maicol.almarales.tamayo@gmail.com>
2025-06-05 17:27:15 -0400

    ok

--- app/Livewire/MenuSecundario.php
+++ app/Livewire/MenuSecundario.php
@@ -7,53 +7,10 @@ use Livewire\Component;
 class MenuSecundario extends Component
 {
     public $nombreAplicacion = 'Escuela Online';
-    public $opcionesMenu = [
-            [
-                'icono' => 'person',
-                'titulo' => 'Personas',
-                'opciones' => [
-                    ['icono' => 'person-gear', 'titulo' => 'Roles', 'areaTrabajo' => 'roles'],
-                    ['icono' => 'people', 'titulo' => 'Usuarios', 'areaTrabajo' => 'usuarios'],
-                    ['icono' => 'person-up', 'titulo' => 'Administradores', 'areaTrabajo' => 'administradores'],
-                    ['icono' => 'person-check', 'titulo' => 'Directivos', 'areaTrabajo' => 'directivos'],
-                    ['icono' => 'person-check', 'titulo' => 'Docentes', 'areaTrabajo' => 'docentes'],
-                    ['icono' => 'person-check', 'titulo' => 'Tutores', 'areaTrabajo' => 'tutores'],
-                    ['icono' => 'person-check', 'titulo' => 'Alumnos', 'areaTrabajo' => 'alumnos'],
-                    ['icono' => 'person-vcard', 'titulo' => 'Contactos', 'areaTrabajo' => 'contactos'],
-                    ['icono' => 'gender-ambiguous', 'titulo' => 'Géneros', 'areaTrabajo' => 'generos']],
-            ],
-            [
-                'icono' => 'mortarboard',
-                'titulo' => 'Estructura',
-                'opciones' => [
-                    ['icono' => '123', 'titulo' => 'Grados', 'areaTrabajo' => 'grados'], 
-                    ['icono' => 'alphabet-uppercase', 'titulo' => 'Secciones', 'areaTrabajo' => 'secciones'], 
-                    ['icono' => 'book', 'titulo' => 'Asignaturas', 'areaTrabajo' => 'asignaturas'], 
-                    ['icono' => 'journal-check', 'titulo' => 'Cursos', 'areaTrabajo' => 'cursos'], 
-                    ['icono' => 'person-workspace', 'titulo' => 'Aulas', 'areaTrabajo' => 'aulas']]
-            ],
-            [
-                'icono' => 'speedometer2',
-                'titulo' => 'Formación',
-                'opciones' => [
-                    ['icono' => 'collection-play', 'titulo' => 'Tipos de recursos', 'areaTrabajo' => 'tiposRecursos'], 
-                    ['icono' => 'file-earmark-binary', 'titulo' => 'Recursos', 'areaTrabajo' => 'recursos'], 
-                    ['icono' => 'list-check', 'titulo' => 'Actividades', 'areaTrabajo' => 'actividades'], 
-                    ['icono' => 'pencil-square', 'titulo' => 'Respuestas', 'areaTrabajo' => 'respuestas'], 
-                    ['icono' => 'megaphone', 'titulo' => 'Anuncios', 'areaTrabajo' => 'anuncios']]
-            ],
-            [
-                'icono' => 'clipboard-check',
-                'titulo' => 'Evaluación',
-                'opciones' => [
-                    ['icono' => '5-circle', 'titulo' => 'Tipos de calificaciones', 'areaTrabajo' => 'tiposCalificaciones'], 
-                    ['icono' => 'patch-check', 'titulo' => 'Calif. cursos', 'areaTrabajo' => 'califCursos']]
-            ]
-        ];
 
-    public function mount()
+    public function mount($nombreAplicacion)
     {
-       
+       $this->nombreAplicacion = $nombreAplicacion;
     }
 
     public function render()
--- resources/views/livewire/menu-secundario.blade.php
+++ resources/views/livewire/menu-secundario.blade.php
@@ -1,29 +1,53 @@
-<aside class="app-sidebar bg-body-secondary shadow" data-bs-theme="light">
-    <!--cabecera-->
-    <div class="sidebar-brand bg-primary">
-        <a href="/" class="brand-link">
-            <!--logo-->
-            <img src="{{ asset('adminLTE/img/logo.png') }}" alt={{ $nombreAplicacion }}
-                class="brand-image shadow rounded-circle" />
+<aside class="main-sidebar sidebar-light-primary elevation-4">
+    <!-- logo + nombre de la aplicación -->
+    <a href="index3.html" class="brand-link">
+        <img src="admin-lte/dist/img/AdminLTELogo.png" alt={{$nombreAplicacion}} class="brand-image img-circle elevation-3"
+            style="opacity: .8">
+        <span class="brand-text font-weight-light">{{$nombreAplicacion}}</span>
+    </a>
 
-            <!--nombre de la aplicación-->
-            <span class="brand-text fw-light text-white">{{ $nombreAplicacion }}</span>
-        </a>
-    </div>
+    <!-- opciones -->
+    <div class="sidebar">
+        <!-- perfil de usuario -->
+        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+            <div class="image">
+                <img src="admin-lte/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+            </div>
+            <div class="info">
+                <a href="#" class="d-block">Maicol Almarales</a>
+            </div>
+        </div>
 
-    <!--opciones-->
-    <div class="sidebar-wrapper">
-        <nav class="mt-2">
-            <ul class="nav sidebar-menu flex-column" data-lte-toggle="treeview" data-accordion="false">
-                @foreach ($opcionesMenu as $opcion)
-                    <livewire:subMenu
-                        :opcionesMenu="$opcionesMenu[$loop->index]"
-                        id="submenu-{{ $loop->index }}"
+        <!-- submenúes -->
+        <nav class="mt-2 nav-light">
+            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                @livewire(
+                'sub-menu',
+                [
+                'cabecera' => ['icono' => 'clipboard-data', 'titulo' => 'Paneles de comando'],
+                'opciones' => [
+                ['icono' => 'people', 'titulo' => 'Personas', 'tituloTablero' => 'Comando de personas'],
+                ['icono' => 'mortarboard', 'titulo' => 'Estructura', 'tituloTablero' => 'Comando de estructura']],
+                'nombreSubMenu' => 'sub-menu-personas'
+                ],
+                key('sub-menu-personas'))
 
-                        wire:key="submenu-{{ $loop->index }}"
-                        wire:ref="submenu-{{ $loop->index }}"
-                    />
-                @endforeach
+                @livewire(
+                'sub-menu',
+                [
+                'cabecera' => ['icono' => 'people', 'titulo' => 'Personas'],
+                'opciones' => [
+                ['icono' => 'person-check', 'titulo' => 'Roles', 'tituloTablero' => 'Tablero Roles'],
+                ['icono' => 'gender-ambiguous', 'titulo' => 'Géneros', 'tituloTablero' => 'Tablero Géneros'],
+                ['icono' => 'person', 'titulo' => 'Usuarios', 'tituloTablero' => 'Tablero Usuarios'],
+                ['icono' => 'person-fill', 'titulo' => 'Administradores', 'tituloTablero' => 'Tablero Administradores'],
+                ['icono' => 'person-fill', 'titulo' => 'Directivos', 'tituloTablero' => 'Tablero Directivos'],
+                ['icono' => 'person-fill', 'titulo' => 'Docentes', 'tituloTablero' => 'Tablero Docentes'],
+                ['icono' => 'person-fill', 'titulo' => 'Tutores', 'tituloTablero' => 'Tablero Tutores'],
+                ['icono' => 'person-fill', 'titulo' => 'Alumnos', 'tituloTablero' => 'Tablero Alumnos']],
+                'nombreSubMenu' => 'sub-menu-personas'
+                ],
+                key('sub-menu-personas'))
             </ul>
         </nav>
     </div>
