d5e1b0cfa8e74d911280196df86e5cc88d8c3c3d
Maicol Almarales Tamayo <maicol.almarales.tamayo@gmail.com>
2025-06-05 17:44:27 -0400

    ok

--- resources/views/livewire/opcion-sub-menu.blade.php
+++ resources/views/livewire/opcion-sub-menu.blade.php
@@ -1,5 +1,5 @@
 <li class="nav-item" wire:click="click">
-    <a href="#" class="nav-link @if ($estado) active @endif">
+    <a href="#" class="nav-link @if ($estado) active bg-primary text-white @endif">
         <i class="nav-icon bi bi-{{ $icono }}"></i>
         <p>{{ $titulo }}</p>
     </a>
