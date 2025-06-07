aa8e956d3235ccc2756ec43770e05c18bbab36e5
Maicol Almarales Tamayo <maicol.almarales.tamayo@gmail.com>
2025-06-06 20:50:49 -0400

    ok

--- resources/views/livewire/area-trabajo.blade.php
+++ resources/views/livewire/area-trabajo.blade.php
@@ -4,7 +4,7 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1 class="m-0"> {{ $tituloAreaTrabajo }} {{$codigoAreaTrabajo }} </h1>
+                    <h1 class="m-0"> {{ $tituloAreaTrabajo }} </h1>
                 </div>
             </div>
         </div>
@@ -12,14 +12,28 @@
 
     @switch($codigoAreaTrabajo)
         @case('00')
-            maicol
+            <div>
+                En construcción
+            </div>
         @break
         
         @case('01')
-            @livewire('TablaUsuarios')
+            @livewire('tabla', 
+                ['titulo' => 'Tabla Usuarios',
+                'modelo' => 'Usuario',
+                'pagina' => 1, 
+                'objetosPagina' => 5])
+        @break
+
+        @case('02')
+            @livewire('tabla', 
+                ['titulo' => 'Tabla Géneros',
+                'modelo' => 'Genero',
+                'pagina' => 1, 
+                'objetosPagina' => 5])
         @break
         
         @default
-            
+            {{--  --}}
     @endswitch
 </div>
--- resources/views/livewire/tabla-usuarios.blade.php
+++
@@ -1,7 +0,0 @@
-@livewire('tabla', 
-    ['titulo' => 'Tabla Usuarios',
-     'modelo' => 'Usuario',
-     'pagina' => 1, 
-     'objetosPagina' => 5, ],
-     
-     key('tabla-usuarios'))
