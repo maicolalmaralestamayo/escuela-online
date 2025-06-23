a8bd0cdc6525ea8a6644da34df66bf1924ca436b
Maicol Almarales Tamayo <maicol.almarales.tamayo@gmail.com>
2025-06-23 13:35:12 -0400

    OK

--- app/Models/Docente.php
+++ app/Models/Docente.php
@@ -21,6 +21,27 @@ class Docente extends Model
         ];
     }
 
+    static public function camposModificables(){
+        return [
+            'nombre_1',
+            'nombre_2',
+            'apellido_1',
+            'apellido_2',
+            'dni',
+            'genero_id',
+            'observacion',
+            'correo',
+            'celular',
+        ];
+    }
+
+    static public function camposNoModificables(){
+        return [
+            'created_at',
+            'updated_at',
+        ];
+    }
+
     public function usuario(): BelongsTo {
         return $this->belongsTo(Usuario::class);
     }
